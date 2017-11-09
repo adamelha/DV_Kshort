0bc12a62c4f38fa50e83c4375e606de97e13537d
Niv Konstantini <nkonstan@lxplus116.cern.ch>
2017-11-09 18:57:34 +0100

    new name and some new changes

--- Kshort_DDL.cxx
+++ Kshort_DDL.cxx
@@ -8,23 +8,23 @@
 #include "StoreGate/StoreGateSvc.h"
 #include "StoreGate/DataHandle.h"
 
-
+				
 // EDM includes: - if move to header file will not compile?
 #include "xAODEventInfo/EventInfo.h"
-#include "xAODTracking/TrackMeasurementValidationContainer.h"
-#include "xAODTracking/TrackStateValidationContainer.h"
+//#include "xAODTracking/TrackMeasurementValidationContainer.h"
+//#include "xAODTracking/TrackStateValidationContainer.h"
 #include "xAODTracking/TrackParticleContainer.h"
-#include "xAODTracking/TrackParticleAuxContainer.h"
+//#include "xAODTracking/TrackParticleAuxContainer.h"
 #include "xAODTracking/VertexContainer.h"
 
 #include "xAODTracking/Vertex.h"
-#include "xAODTracking/VertexAuxContainer.h"
+//#include "xAODTracking/VertexAuxContainer.h"
 
-#include "xAODTruth/TruthParticleContainer.h"
-#include "xAODTruth/TruthParticle.h"
-#include "xAODTruth/TruthVertex.h"
-#include "xAODTruth/TruthVertexContainer.h"
-#include "xAODTruth/xAODTruthHelpers.h"
+//#include "xAODTruth/TruthParticleContainer.h"
+//#include "xAODTruth/TruthParticle.h"
+//#include "xAODTruth/TruthVertex.h"
+//#include "xAODTruth/TruthVertexContainer.h"
+//#include "xAODTruth/xAODTruthHelpers.h"
 
 #include "StoreGate/StoreGate.h"
 #include "StoreGate/StoreGateSvc.h"
@@ -38,11 +38,9 @@
 
 
 //ROOT
-#include "TH1I.h"
-#include "TH1F.h"
-#include "TH2F.h"
 #include "TEfficiency.h"
 #include "TFile.h"
+
 
 #include "GaudiKernel/ITHistSvc.h"
 
@@ -56,65 +54,85 @@
 #include <functional>
 #include <cmath>
 
+
+
 DDL::Kshort_DDL::Kshort_DDL( const std::string& name, ISvcLocator* pSvcLocator ) : 
    AthAnalysisAlgorithm( name, pSvcLocator ),
    m_nTracks(0)   
 {
 
-  declareProperty( "Property", m_nProperty ); //example property declaration
+  //declareProperty("HistSvcName", m_hist_name = "Kshort_DDL" ); 
 
 }
 
-
-// Kshort_DDL::~Kshort_DDL() {}
-
-
 StatusCode DDL::Kshort_DDL::initialize() { 
-
-  using namespace std;
   
-  StatusCode sc = service("StoreGateSvc", m_storeGate);
-  if (sc.isFailure()) {
-     msg(MSG::ERROR) << "Unable to retrieve pointer to StoreGateSvc" << endreq;
-     return sc;
-  }
+	StatusCode sc = service("StoreGateSvc", m_storeGate);
+	if (sc.isFailure()) 
+	{
+		msg(MSG::ERROR) << "Unable to retrieve pointer to StoreGateSvc" << endreq;
+		return sc;
+	}
 
-  sc = m_trigDec.retrieve();
-  if (sc.isFailure()){
-     msg(MSG::ERROR) << "Can't get handle on TrigDecisionTool" << endreq;
-  }
+	sc = m_trigDec.retrieve();
+	if (sc.isFailure())
+	{
+		msg(MSG::ERROR) << "Can't get handle on TrigDecisionTool" << endreq;
+	}
 
-  sc = service("THistSvc", m_thistSvc);
-  if (sc.isFailure()) {
-     msg(MSG::ERROR) << "Unable to retrieve pointer to THistSvc" << endreq;
-     return sc;
-  }
+	sc = service("THistSvc", m_thistSvc);
+	if (sc.isFailure()) {
+		msg(MSG::ERROR) << "Unable to retrieve pointer to THistSvc" << endreq;
+		return sc;
+	}
 
-  // Histogramming...
-  ServiceHandle<ITHistSvc> histSvc("THistSvc",name());
-  CHECK( histSvc.retrieve() );
+	// Histogramming...
+	ServiceHandle<ITHistSvc> histSvc("THistSvc",name());
+	CHECK( histSvc.retrieve() );
 
-  if (sc.isFailure()) {
-     msg(MSG::ERROR) << "Unable to register histogramming service" << endreq;
-     return sc;
-  }
+	if (sc.isFailure()) 
+	{
+		msg(MSG::ERROR) << "Unable to register histogramming service" << endreq;
+		return sc;
+	}
 
-  // Creation of new histograms : pi+, pi-, Kshort histograms
-  h_Name = new TH1I("h_Name", "; Title",n_bins,n_min,n_max);
-  h_Name2 = new TH2F("h_Name2", "; Title1; Title2", n_bins1,n_min1,n_max1,n_bins2,n_min2,n_max2);
- 
-  // Efficiency histograms
-  h_Name_Eff = new TH1F("h_Name_Eff", " Quantity1 vs. Quantity2 ", bins, q_min, q_max);
 
-  // Truth particle histograms 
- 
-  // Vertex Histograms
- 
-  // Cutflow histograms      
+	//// ATH_MSG_INFO ("Initializing " << name() << "...");
 
-  //// ATH_MSG_INFO ("Initializing " << name() << "...");
+	// Create a tree
+	//m_Ks_tree = new TTree("Kshort_DDL", "");
 
-  return StatusCode::SUCCESS;
+	//m_vxtree->SetTree(m_Ks_tree);
+	// Register the tree
+	//std::string hist_path = "/" + m_hist_name + "/";
+	//CHECK(m_histSvc->regTree(hist_path + m_Ks_tree->GetName(), m_Ks_tree));
+
+
+	*kshort_tree = new TTree("KshortTTree","Kshort Tree!");
+
+
+	kshort_tree->Branch("piplus_pt",&piplus_pt,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_p,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_px,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_py,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_pz,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_e,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_z0,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_d0,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piplus_eta,"piplus_pt/f");
+	
+	kshort_tree->Branch("piplus_pt",&piminus_pt,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_p,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_px,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_py,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_pz,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_e,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_z0,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_d0,"piplus_pt/f");
+	kshort_tree->Branch("piplus_pt",&piminus_eta,"piplus_pt/f");
+
+
+	return StatusCode::SUCCESS;
 }
 
 StatusCode DDL::Kshort_DDL::finalize() {
@@ -124,85 +142,174 @@ StatusCode DDL::Kshort_DDL::finalize() {
   return StatusCode::SUCCESS;
 }
 
-StatusCode DDL::Kshort_DDL::execute() {
-      StatusCode sc = StatusCode::SUCCESS;
-      sc = fillKs();
-      if ( sc.isFailure() ) {
-         msg(MSG::ERROR) << "Ks filling failed" << endreq;
-         return StatusCode::SUCCESS;
-      }  
+StatusCode DDL::Kshort_DDL::execute() 
+{
+	this->event_counter++;
+	std::cout << "Event Number : " << this->event_counter << std::endl;
+	StatusCode sc = StatusCode::SUCCESS;
+    sc = fillKs();
+    if ( sc.isFailure() ) 
+	{
+       msg(MSG::ERROR) << "Ks filling failed" << endreq;
+       return StatusCode::SUCCESS;
+    }  
       return sc;     
   //// ATH_MSG_DEBUG ("Executing " << name() << "...");
 }
 
 
-StatusCode DDL::Kshort_DDL::fillKs() {
+// Function for charged pion mass (equating float to some number inside a condition is not recommended, hence this function was made) 
+bool DDL::Kshort_DDL::isPi(float piMass)
+{
+        float actual_pi_mass = 139.57061; // MeV (taken from recent PDG, http://pdglive.lbl.gov/Particle.action?init=0&node=S008)
+        float margin_of_error = 0.00024; // MeV (taken from recent PDG, http://pdglive.lbl.gov/Particle.action?init=0&node=S008)
 
-  using namespace std;
-  int counterEvent = 0;
-  StatusCode sc = StatusCode::SUCCESS; 
+        return ( (piMass >= (actual_pi_mass - margin_of_error)) &&  (piMass <= (actual_pi_mass + margin_of_error)) );
+}
+
+
+StatusCode DDL::Kshort_DDL::finding_right_ks() {
+
+	StatusCode sc = StatusCode::SUCCESS; 
 
 //----------------------------------------------------------------------------
 
+
+	const xAOD::VertexContainer* vertices = nullptr;
+	CHECK(evtStore()->retrieve(vertices, "VrtSecIncludive_SecondaryVertices"); // Is VrtSecIncludive_SecondaryVertices OK or any name can replace it ?
+  
+	// "TrackParticle": pointer to a given track that was used in vertex reconstruction
+	xAOD::TrackParticle* piplus_track = nullptr;
+	xAOD::TrackParticle* piminus_track = nullptr;
+
+	// Vertex container for Kshorts
+	xAOD::VertexContainer* kshortVertices = new VertexContainer();
+  
+  
+	// Looping over the vertices to find Kshort vertices which decay to pi+ and pi-
+	// Defining the iterator in a short and efficient way
+	for(xAOD::Vertex* vertex_ptr : vertices)
+	{
+        // Vertices with only two tracks 
+		if(vertex_ptr->nTrackParticles()==2)
+		{
+			piplus_track = vertex_ptr->trackParticle(0);
+			piminus_track = vertex_ptr->trackParticle(1);
+			if(piplus_track->charge()<piminus_track->charge()) //in case track 0 is piMinus and track 1 is piPlus
+			{
+				std::swap(piminus_track,piplus_track);
+			}
+			// Checking whether the masses are equal to the charged pion mass
+			if( isPi(piplus_track->m()) && isPi(piminus_track->m()) )
+			{
+				// Checking if they have charges of -1 and +1
+				if(piplus_track->charge()+piminus_track->charge()==0 && piplus_track->charge()==1)
+                {      
+					// How to save the mass of such vertices and other parameters ?
+					// The idea is to save all these variables in form of a Tree.	
+					// Mass of the Kshort vertices after confirming that they are indeed Kshort vertices
+					// Also a storage for the Kshort vertices
+					//KshortVertices->push_back(vertex_ptr); //not sure we need it
+   				
+
+					
+					// Saving individual track info in ttree
+					
+					piplus_pt 	= 	piplus_track->pt();
+					piplus_p  	= 	piplus_track->p();
+					piplus_px	= 	piplus_track->p4().px();
+					piplus_py	= 	piplus_track->p4().py();
+					piplus_pz	= 	piplus_track->p4().pz();
+					piplus_e	= 	piplus_track->e();
+					piplus_z0	=	piplus_track->z0();
+					piplus_d0	=	piplus_track->d0();
+					piplus_eta	=	piplus_track->eta();
+
+					piminus_pt 	= 	piminus_track->pt();
+					piminus_p  	= 	piminus_track->p();
+					piminus_px	= 	piminus_track->p4().px();
+					piminus_py	= 	piminus_track->p4().py();
+					piminus_pz	= 	piminus_track->p4().pz();
+					piminus_e	= 	piminus_track->e();
+					piminus_z0	=	piminus_track->z0();
+					piminus_d0	=	piminus_track->d0();
+					piminus_eta	=	piminus_track->eta();
+
+					kshort_tree->Fill();
+				
+				}
+			}			
+		}	
+	}
+	
+
+
   // Track container for all tracks 
+  /*
   const xAOD::TrackParticleContainer* recoTracks = 0;
   CHECK( evtStore()->retrieve( recoTracks, "InDetTrackParticles" ) ); 
-
-  // pi+ tracks
-  const xAOD::TrackParticleContainer* recoTracks_piplus = 0;
-  CHECK( evtStore()->retrieve( recoTracks_piplus, "InDetTrackParticles" ) );
-  if ((*recoTracks)->TrackParticle()->m()==0.1396 && (*recoTracks)->TrackParticle()->charge()==1 ){
-      recoTracks_piplus = *recoTracks;  
-  }
   
- 
-  // pi- tracks
-  const xAOD::TrackParticleContainer* recoTracks_piminus = 0;
-  CHECK( evtStore()->retrieve( recoTracks_piminus, "InDetTrackParticles" ) );
-  if ((*recoTracks)->TrackParticle()->m()==0.1396 && (*recoTracks)->TrackParticle()->charge()==-1 ){
-      recoTracks_piminus = *recoTracks;
+  // pi+ tracks
+  xAOD::TrackParticleContainer* piplus_tracks = new TrackParticleContainer();
+  xAOD::TrackParticleContainer* piminus_tracks = new TrackParticleContainer();
+  //CHECK( evtStore()->retrieve( recoTracks_piplus, "InDetTrackParticles" ) );
+  for(xAOD:TrackParticle* track : recoTracks)
+  {
+	if(track->m()==0.1396))
+	{
+		if(track->charge()==1)
+		{
+			piplus_tracks.push_back(track);
+		}
+		else if(track->charge()==-1)
+		{
+			pminus_tracks.push_back(track);
+		}
+		
+	}
   } 
-
+  */
 
   // How to combine 'recoTracks_piplus' and 'recoTracks_piminus'
 
  //----------------------------------------------------------------------------
 
   // Vertex container 
-  const xAOD::VertexContainer* vertexContainer = 0;
-  StatusCode sc=m_storeGate->retrieve(vertexContainer, "VrtSecInclusive_SecondaryVertices");//If RRV sample
-  if(sc==StatusCode::FAILURE)return;
 
  //----------------------------------------------------------------------------
 
   // Truth particle container
-  const xAOD::TruthParticleContainer* truthParticles;
+	/*
+	const xAOD::TruthParticleContainer* truthParticles;
 
-  isMC = false;
+	isMC = false;
     sc=m_storeGate->retrieve(truthParticles, "TruthParticles");
-    if(!sc.isFailure()){  
+    if(!sc.isFailure())
+	{  
       isMC = true;
     }
-
-  // Number of all reconstructed tracks
-  m_nTracks->Fill(recoTracks->size());
-  // Number of reconstructed pi+ tracks
+	*/
+	/*
+	// Number of all reconstructed tracks
+	m_nTracks->Fill(recoTracks->size());
+	// Number of reconstructed pi+ tracks
   
-  // Number of reconstructed pi- tracks
+	// Number of reconstructed pi- tracks
 
-  // How to get to two vertices of pi+ and pi- using VertexContainer
-  double counterDV = 0; 
-  float vtxDist = 0;
-  float vtxMass = -999;
-  double vtxX = -999; 
-  double vtxY = -999; 
-  double vtxZ = -999;
-  double vtxType = -999; 
-  double vtxNbTracks = -999; // number of tracks that make the vertex 
-  double vtxNbTrackParticles = -999; 
+	// How to get to two vertices of pi+ and pi- using VertexContainer
+	double counterDV = 0; 
+	float vtxDist = 0;
+	float vtxMass = -999;
+	double vtxX = -999; 
+	double vtxY = -999; 
+	double vtxZ = -999;
+	double vtxType = -999; 
+	double vtxNbTracks = -999; // number of tracks that make the vertex 
+	double vtxNbTrackParticles = -999; 
   
-  for(xAOD::VertexContainer::const_iterator rVtx_itr = vertexContainer->begin();rVtx_itr != vertexContainer->end(); ++rVtx_itr){//Loop on RECO vertex
-     
+	for(xAOD::VertexContainer::const_iterator rVtx_itr = vertexContainer->begin();rVtx_itr != vertexContainer->end(); ++rVtx_itr)
+	{//Loop on RECO vertex
+
       const xAOD::Vertex *dv = (*rVtx_itr);
       vtxDist = sqrt(((*rVtx_itr)->x()*(*rVtx_itr)->x())+((*rVtx_itr)->y()*(*rVtx_itr)->y()));
       vtxMass = (*rVtx_itr)->auxdataConst<float>("vtx_mass");//Works only on RRV ?
@@ -221,46 +328,11 @@ StatusCode DDL::Kshort_DDL::fillKs() {
       if((*rVtx_itr)->nTrackParticles()>2){continue;}//If vtx has more than 2 tracks, stop.
 
     }
-
-   double counterKs = 0;
-   for(size_t tk = 0; tk < (*rVtx_itr)->nTrackParticles(); ++tk){//loop over all the tracks from the recoVertex
-     
-
-   }
-
-}
-
-// Do we really need this? 
-// Looping on tracks
-void DDL::Kshort_DDL::fillTrack(){
-
-  // Get to the track container 
-  const xAOD::TrackParticleContainer* recoTracks = 0;
-  
-  StatusCode sc=m_storeGate->retrieve(recoTracks, "InDetTrackParticles");
-
-  // obtain pi+ tracks, make one loop ; then obtain pi- tracks, make anothe loop in the pi+ loop
-  for(xAOD::TrackParticleContainer::const_iterator recoTrk_itr = recoTracks->begin();recoTrk_itr != recoTracks->end(); recoTrk_itr++){ 
-
-    for(xAOD::TrackParticleContainer::const_iterator recoTrk_itr2 = recoTrk_itr+1;recoTrk_itr2 != recoTrk_itr->end(); recoTrk_itr2++){
-
-    } 
-    (*recoTrk_itr)->TrackParticle()->charge();
-    (*recoTrk_itr)->TrackParticle()->m();
-    (*recoTrk_itr)->TrackParticle()->m2();
-    (*recoTrk_itr)->TrackParticle()->px();
-    (*recoTrk_itr)->TrackParticle()->py();
-    (*recoTrk_itr)->TrackParticle()->pz();
-    (*recoTrk_itr)->TrackParticle()->p();
-    (*recoTrk_itr)->TrackParticle()->p2();
-    (*recoTrk_itr)->TrackParticle()->eta();
-    (*recoTrk_itr)->TrackParticle()->e();
-    (*recoTrk_itr)->TrackParticle()->phi();
-    (*recoTrk_itr)->TrackParticle()->pt();
-    (*recoTrk_itr)->TrackParticle()->reconstructedVertex();          
-
-  }
-
-
-
+	*/
+	/*
+	double counterKs = 0;
+	for(size_t tk = 0; tk < (*rVtx_itr)->nTrackParticles(); ++tk)
+	{//loop over all the tracks from the recoVertex
+	}	
+	*/
 }
--- Kshort_DDL.h
+++ Kshort_DDL.h
@@ -1,7 +1,7 @@
 #ifndef DDLSTUDIES_KSHORT_DDL_H
 #define DDLSTUDIES_KSHORT_DDL_H 
 
-#include "AthenaBaseComps/AthAnalysisAlgorithm.h"
+#include "AthenaAnalysisBaseComps/AthAnalysisAlgorithm.h"
 #include "GaudiKernel/ToolHandle.h"
 #include "GaudiKernel/ITHistSvc.h"
 #include "StoreGate/StoreGateSvc.h" 
@@ -16,12 +16,16 @@
 #include "xAODTracking/VertexContainer.h"
 #include "xAODTracking/Vertex.h"
 #include "TTree.h"
+#include "TBrowser.h"
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TROOT.h"
+
+using namespace std;
 
 // forward declaration
-class TH1I;
-class TH1F;
-class TH2F;
-class TH1D;
+class TTree;
 class TEfficiency;
 
 
@@ -31,19 +35,51 @@ namespace DDL {
     class Kshort_DDL: public ::AthAnalysisAlgorithm 
     {
  
+		private: 
+			// histogram service
+        	//ServiceHandle<ITHistSvc> m_histSvc;
+        	//std::string m_hist_name;
+
+			int event_counter=0;
+
+			//TTree* m_Ks_tree;
+			TTree* kshort_tree = nullptr;
+
+			//variables for the TTREE
+			double piplus_pt=0; 	// transverse momentum of pi+ track
+			double piplus_p=0; 	// momentum magnitude of pi+ track
+			double piplus_px=0;	// x component of momentum of pi+ track
+			double piplus_py=0;	// y component of momentum of pi+ track
+			double piplus_pz=0;	// z component of momentum of pi+ track
+			double piplus_e=0;	// energy of pi+ track
+			double piplus_z0=0;	// z0 of pi+ track 
+			double piplus_d0=0;	// d0 of pi+ track
+			double piplus_eta=0;	// pseudo rapidity of pi+ track 
+
+			double piminus_pt=0;	// transverse momentum of pi- track
+			double piminus_p=0;	// momentum magnitude of pi- track
+			double piminus_px=0;	// x component of momentum of pi- track
+			double piminus_py=0;	// y component of momentum of pi- track
+			double piminus_pz=0;	// z component of momentum of pi- track
+			double piminus_e=0;	// energy of pi- track
+			double piminus_z0=0;	// z0 of pi- track 
+			double piminus_d0=0;	// d0 of pi- track
+			double piminus_eta=0;	// pseudo rapidity of pi- track 
+
+
+
         public: 
-            Kshort_DDL( const std::string& name, ISvcLocator* pSvcLocator );
-            virtual ~Kshort_DDL() = default; 
+            	Kshort_DDL( const std::string& name, ISvcLocator* pSvcLocator );
+            	virtual ~Kshort_DDL() = default; 
 
-            StatusCode  initialize() override;
-            StatusCode  execute() override;
-            StatusCode  finalize() override;
+            	StatusCode  initialize() override;
+            	StatusCode  execute() override;
+            	StatusCode  finalize() override;
 
-            // for good Kshort ?
-            virtual bool goodKs () const;
-
-        private: 
-
+            	// for good Kshort ?
+            	//virtual bool goodKs () const;
+				bool isPi(float piMass);
+				StatusCode finding_right_ks();
    };
 
 } 
